c1c89900913b1ff25d7ca5f37f108ee0f7a989b6
Tyler Biethman <tbiethman@gmail.com>
2017-08-02 01:01:07 -0500

    What the heck

--- packages/terra-clinical-application-site/src/demo/navigation-application/orders/Orders.jsx
+++ packages/terra-clinical-application-site/src/demo/navigation-application/orders/Orders.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Orders = () => (
+const Orders = ({ routingManager }) => (
   <div style={{ height: '100%', width: '100%', position: 'absolute', backgroundColor: 'lightgrey' }}>
     <h2>Orders</h2>
+    <h3>I am {routingManager.size}</h3>
   </div>
 );
 
