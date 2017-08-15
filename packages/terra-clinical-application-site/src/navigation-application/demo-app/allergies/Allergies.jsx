fb26fa5f891a78c9041fa437fd56a99b221f9d56
Tyler Biethman <tbiethman@gmail.com>
2017-08-15 14:05:04 -0500

    Removing older navigation demo.

--- packages/terra-clinical-application-site/src/navigation-application/demo-app/allergies/Allergies.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Allergies = ({ match, location }) => (
-  <div style={{ height: '100%', width: '100%', position: 'absolute', backgroundColor: 'lightgrey' }}>
-    <h2>Allergies</h2>
-  </div>
-);
-
-export default Allergies;
--- packages/terra-clinical-application-site/src/navigation-application/demo-app/home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => (
-  <div style={{ height: '100%', width: '100%', position: 'absolute', backgroundColor: 'lightgrey' }}>
-    <h2>HOME</h2>
-  </div>
-);
-
-export default Home;
