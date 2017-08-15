194d8dd3bd656706119e5e7b756fe1e36f4ba18c
Tyler Biethman <tbiethman@gmail.com>
2017-08-15 12:45:55 -0500

    Cleaning up Chart too

--- packages/terra-clinical-application-site/src/navigation-application/powerchart-web/patient-context/chart/Chart.jsx
+++ packages/terra-clinical-application-site/src/navigation-application/powerchart-web/patient-context/chart/Chart.jsx
@@ -1,57 +1,53 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ChartReview from './review/ChartReview';
 
-class Chart extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.redirect = true;
-  }
-
-  componentDidMount() {
-    this.redirect = false;
-  }
-
-  render() {
-    return (
-      <div style={{ height: '100%', width: '100%', position: 'absolute', backgroundColor: 'fuschia' }}>
-        <Switch>
-          <Route
-            path="/patients/chart/review"
-            render={props => (
-              <ChartReview routingManager={this.props.routingManager} app={this.props.app} {...props} />
-            )}
-          />
-          <Route
-            path="/patients/chart/orders"
-            render={() => (
-              <div style={{ height: '100%' }}>
-                <div style={{ height: '100%', backgroundColor: 'lightgrey', position: 'relative' }}>
-                  <div style={{ position: 'absolute', top: '50%', left: '50%', color: 'grey', transform: 'translateX(-50%)' }}>
-                    <h2>No Orders</h2>
-                  </div>
-                </div>
+const Chart = ({ routingManager, app }) => (
+  <div style={{ height: '100%', width: '100%', position: 'absolute', backgroundColor: 'fuschia' }}>
+    <Switch>
+      <Route
+        path="/patients/chart/review"
+        render={props => (
+          <ChartReview routingManager={routingManager} app={app} {...props} />
+        )}
+      />
+      <Route
+        path="/patients/chart/orders"
+        render={() => (
+          <div style={{ height: '100%' }}>
+            <div style={{ height: '100%', backgroundColor: 'lightgrey', position: 'relative' }}>
+              <div style={{ position: 'absolute', top: '50%', left: '50%', color: 'grey', transform: 'translateX(-50%)' }}>
+                <h2>No Orders</h2>
               </div>
-            )}
-          />
-          <Route
-            path="/patients/chart/documents"
-            render={() => (
-              <div style={{ height: '100%' }}>
-                <div style={{ height: '100%', backgroundColor: 'lightgrey', position: 'relative' }}>
-                  <div style={{ position: 'absolute', top: '50%', left: '50%', color: 'grey', transform: 'translateX(-50%)' }}>
-                    <h2>No Documents</h2>
-                  </div>
-                </div>
+            </div>
+          </div>
+        )}
+      />
+      <Route
+        path="/patients/chart/documents"
+        render={() => (
+          <div style={{ height: '100%' }}>
+            <div style={{ height: '100%', backgroundColor: 'lightgrey', position: 'relative' }}>
+              <div style={{ position: 'absolute', top: '50%', left: '50%', color: 'grey', transform: 'translateX(-50%)' }}>
+                <h2>No Documents</h2>
               </div>
-            )}
-          />
-          {this.redirect && <Redirect to="/patients/chart/review" />}
-        </Switch>
-      </div>
-    );
-  }
-}
+            </div>
+          </div>
+        )}
+      />
+      <Route
+        render={() => (
+          <div style={{ height: '100%' }}>
+            <div style={{ height: '100%', backgroundColor: 'lightgrey', position: 'relative' }}>
+              <div style={{ position: 'absolute', top: '50%', left: '50%', color: 'grey', transform: 'translateX(-50%)' }}>
+                <h2>Select a Chart Section</h2>
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    </Switch>
+  </div>
+);
 
 export default Chart;
