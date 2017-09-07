d0db116930533624ec6a2992b2933f5ccfc67203
Kasper, Dave <dave.m.kasper@gmail.com>
2017-09-07 13:18:37 -0500

    Improve patient selection.

--- packages/terra-clinical-application-site/src/navigation-application/powerchart-web/patient-context/patient-schedule/PatientSchedule.jsx
+++ packages/terra-clinical-application-site/src/navigation-application/powerchart-web/patient-context/patient-schedule/PatientSchedule.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Button from 'terra-button';
+import SingleSelectList from 'terra-list/lib/SingleSelectList';
+import ItemView from 'terra-clinical-item-view';
+import ItemDisplay from 'terra-clinical-item-display';
 
 class PatientSchedule extends React.Component {
   static selectPatient(patientData) {
@@ -25,22 +28,56 @@ class PatientSchedule extends React.Component {
       );
     }
 
+    const rambo = (
+      <ItemView
+        displays={[
+          <ItemDisplay text="Rambo, John" />,
+          <ItemDisplay text="Care Position: Primary" />,
+          <ItemDisplay text="Room 100A" />,
+          <ItemDisplay text="Acuity: 5" />,
+          <ItemDisplay text="Start Time: 08-05-2016 12:00:00" />,
+          <ItemDisplay text="End Time: 08-05-2016 16:00:00" />,
+        ]}
+        layout="twoColumns"
+      />
+    );
+
+    const williams = (
+      <ItemView
+        displays={[
+          <ItemDisplay text="Williams, Ash" />,
+          <ItemDisplay text="Care Position: Primary" />,
+          <ItemDisplay text="Room 101B" />,
+          <ItemDisplay text="Acuity: 5" />,
+          <ItemDisplay text="Start Time: 08-05-2016 17:00:00" />,
+          <ItemDisplay text="End Time: 08-05-2016 18:00:00" />,
+        ]}
+        layout="twoColumns"
+      />
+    );
+
+    const johnson = (
+      <ItemView
+        displays={[
+          <ItemDisplay text="Johnson, Don" />,
+          <ItemDisplay text="Care Position: Primary" />,
+          <ItemDisplay text="Room 103A" />,
+          <ItemDisplay text="Acuity: 4" />,
+          <ItemDisplay text="Start Time: 08-05-2016 18:00:00" />,
+          <ItemDisplay text="End Time: 08-05-2016 19:00:00" />,
+        ]}
+        layout="twoColumns"
+      />
+    );
+
     return (
       <div style={{ padding: '10px' }}>
         <h3>Patient Schedule</h3>
-        <hr />
-        <Button
-          text="Select: John Rambo"
-          onClick={PatientSchedule.selectPatient({ id: 3, name: 'Rambo, John' })}
-        />
-        <Button
-          text="Select: Ash Williams"
-          onClick={PatientSchedule.selectPatient({ id: 2, name: 'Williams, Ash' })}
-        />
-        <Button
-          text="Select: Don Johnson"
-          onClick={PatientSchedule.selectPatient({ id: 1, name: 'Johnson, Don' })}
-        />
+        <SingleSelectList isDivided={true} hasChevrons={false}>
+          <SingleSelectList.Item content={rambo} key="rambo" onClick={PatientSchedule.selectPatient({ id: 3, name: 'Rambo, John' })} />
+          <SingleSelectList.Item content={williams} key="williams" onClick={PatientSchedule.selectPatient({ id: 2, name: 'Williams, Ash' })} />
+          <SingleSelectList.Item content={johnson} key="johnson" onClick={PatientSchedule.selectPatient({ id: 1, name: 'Johnson, Don' })} />
+        </SingleSelectList>
         <br />
         {closeButton}
       </div>
