91e5a26d1ef83dbebb96c3f8da887ea1d8d9cf8a
Kasper, Dave <dave.m.kasper@gmail.com>
2017-10-20 12:32:59 -0500

    Start gutting navigation.

--- packages/terra-clinical-navigation/src/Navigation.jsx
+++ packages/terra-clinical-navigation/src/Navigation.jsx
@@ -1,100 +1,234 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import {
+  withRouter,
+} from 'react-router-dom';
+
 import AppDelegate from 'terra-app-delegate';
-import NavManagerDelegate from 'terra-clinical-navigation-manager/lib/NavManagerDelegate';
+import breakpoints from 'terra-responsive-element/lib/breakpoints.scss';
+import ContentContainer from 'terra-content-container';
+import { navigationConfigPropType, configHasMatchingRoute } from './RoutingConfigUtils';
+import Layout from 'terra-clinical-layout';
 
-import 'terra-base/lib/baseStyles';
-import './Navigation.scss';
-
-const BREAKPOINTS = [
-  'tiny',
-  'small',
-  'medium',
-  'large',
-  'huge',
-];
+import MenuHeader from './menu-header/MenuHeader';
 
 const propTypes = {
+  routeConfig: navigationConfigPropType,
+  location: PropTypes.object,
+  forceToggleAvailable: PropTypes.bool, // Need this to expose toggle when router is not used with layout
   app: AppDelegate.propType,
-  children: PropTypes.node,
-  contentParent: PropTypes.element,
-  menuBreakpoint: PropTypes.oneOf(BREAKPOINTS),
-  menuClass: PropTypes.func,
-  menuProps: PropTypes.object,
-  navManager: NavManagerDelegate.propType,
-};
-
-const defaultProps = {
-  menuBreakpoint: 'tiny',
+  applicationToolbar: PropTypes.element,
+  menuRoutingVessel: PropTypes.element,
+  contentRoutingVessel: PropTypes.element,
 };
 
 class Navigation extends React.Component {
+  static getBreakpointSize() {
+    const width = window.innerWidth;
+    const { small, medium, large, huge } = breakpoints;
+
+    if (width >= 1440) {
+      return 'huge';
+    } else if (width >= 1216) {
+      return 'large';
+    } else if (width >= 992) {
+      return 'medium';
+    } else if (width >= 768) {
+      return 'small';
+    }
+    return 'tiny';
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.togglePin = this.togglePin.bind(this);
+    this.updateSize = this.updateSize.bind(this);
+    this.isCompactLayout = this.isCompactLayout.bind(this);
+    this.renderApplicationToolbar = this.renderApplicationToolbar.bind(this);
+    this.renderMenu = this.renderMenu.bind(this);
+    this.renderContent = this.renderContent.bind(this);
+
+    const initialSize = Navigation.getBreakpointSize();
+
+    this.state = {
+      menuIsOpen: false,
+      menuIsPinned: false,
+      toggleIsAvailable: configHasMatchingRoute(props.location.pathname, props.routeConfig.menuRoutes, initialSize) || props.forceToggleAvailable,
+      size: initialSize,
+    };
+  }
 
   componentDidMount() {
-    if (this.props.navManager.registerNavigation) {
-      const menuData = {
-        breakpoint: this.props.menuBreakpoint,
-        class: this.props.menuClass,
-        props: this.props.menuProps,
-      };
-      this.props.navManager.registerNavigation(this.props.navManager.index, menuData);
+    window.addEventListener('resize', this.updateSize);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const toggleIsAvailable = configHasMatchingRoute(nextProps.location.pathname, nextProps.routeConfig.menuRoutes, this.state.size) || nextProps.forceToggleAvailable;
+
+    if (toggleIsAvailable !== this.state.toggleIsAvailable) {
+      this.setState({
+        toggleIsAvailable,
+        menuIsOpen: toggleIsAvailable && this.state.menuIsOpen,
+        menuIsPinned: toggleIsAvailable && this.state.menuIsPinned,
+      });
     }
   }
 
   componentWillUnmount() {
-    if (this.props.navManager.deregisterNavigation) {
-      this.props.navManager.deregisterNavigation(this.props.navManager.index);
+    window.removeEventListener('resize', this.updateSize);
+  }
+
+  updateSize() {
+    const newSize = RoutingManager.getBreakpointSize();
+
+    if (this.state.size !== newSize) {
+      const newToggleIsAvailable = configHasMatchingRoute(this.props.location.pathname, this.props.routeConfig.menuRoutes, newSize) || this.props.forceToggleAvailable;
+
+      const newMenuIsPinned = newToggleIsAvailable && (newSize !== 'tiny' && newSize !== 'small') && this.state.menuIsPinned;
+
+      const newMenuIsOpen = newToggleIsAvailable && this.state.menuIsOpen && newMenuIsPinned;
+      this.setState({
+        size: newSize,
+        toggleIsAvailable: newToggleIsAvailable,
+        menuIsOpen: newMenuIsOpen,
+        menuIsPinned: newMenuIsPinned,
+      });
     }
   }
 
-  render() {
-    const {
+  toggleMenu() {
+    if (!this.state.menuIsPinned) {
+      this.setState({
+        menuIsOpen: !this.state.menuIsOpen,
+      });
+    }
+  }
+
+  togglePin() {
+    this.setState({
+      menuIsPinned: !this.state.menuIsPinned,
+    });
+  }
+
+  isCompactLayout() {
+    return this.state.size === 'tiny' || this.state.size === 'small';
+  }
+
+  renderApplicationToolbar() {
+    const { app, routeConfig, applicationToolbar } = this.props;
+    const { size, menuIsOpen } = this.state;
+
+    if (!applicationToolbar) {
+      return null;
+    }
+
+    const isCompactLayout = this.isCompactLayout();
+    const shouldDisplayMenuToggle = isCompactLayout || this.state.toggleIsAvailable;
+
+    return React.cloneElement(applicationToolbar, {
       app,
-      children,
-      contentParent,
-      navManager,
-      ...customProps
-    } = this.props;
+      routingManager: {
+        size,
+        location,
+        routeConfig,
+        isCompactLayout,
+        toggleMenu: shouldDisplayMenuToggle && this.toggleMenu,
+        menuIsOpen,
+      },
+      onToggleClick: shouldDisplayMenuToggle ? this.toggleMenu : undefined,
+    });
+  }
 
-    const navigationClassNames = classNames([
-      'terraClinical-Navigation',
-      customProps.className,
-    ]);
+  renderMenu() {
+    const { app, routeConfig, menuRoutingVessel } = this.props;
+    const { size, menuIsOpen, menuIsPinned } = this.state;
 
-    const newManager = Object.assign({}, navManager, { index: navManager.index + 1 });
-
-    let childContent;
-    if (children) {
-      const newChildProps = {
-        app,
-        navManager: newManager,
-      };
-
-      childContent = React.Children.map(children, child => (
-        React.cloneElement(child, newChildProps)
-      ));
+    if (!menuRoutingVessel) {
+      return null;
     }
 
-    if (contentParent) {
-      const newParentProps = {
-        app,
-        children: childContent,
-        navManager: newManager,
-      };
-      childContent = React.cloneElement(contentParent, newParentProps);
-    }
+    const isCompactLayout = this.isCompactLayout();
+    const vessel = React.cloneElement(menuRoutingVessel, {
+      app,
+      routingManager: {
+        size,
+        location,
+        routeConfig,
+        isCompactLayout,
+        toggleMenu: this.toggleMenu,
+        menuIsOpen,
+        togglePin: !isCompactLayout ? this.togglePin : undefined,
+        menuIsPinned: !isCompactLayout ? menuIsPinned : undefined,
+      },
+    });
 
     return (
-      <div {...customProps} className={navigationClassNames}>
-        {childContent}
-      </div>
+      <ContentContainer
+        fill
+        header={!isCompactLayout ? (
+          <MenuHeader
+            text="Menu"
+            togglePin={!isCompactLayout ? this.togglePin : undefined}
+            isPinned={!isCompactLayout ? menuIsPinned : undefined}
+          />
+        ) : undefined}
+      >
+        {vessel}
+      </ContentContainer>
+    );
+  }
+
+  renderContent() {
+    const { app, routeConfig, contentRoutingVessel } = this.props;
+    const { size, menuIsOpen } = this.state;
+
+    if (!contentRoutingVessel) {
+      return null;
+    }
+
+    const isCompactLayout = this.isCompactLayout();
+
+    return (
+      <ContentContainer
+        fill
+        header={isCompactLayout && this.renderApplicationToolbar()}
+      >
+        {(
+          React.cloneElement(contentRoutingVessel, {
+            app,
+            routingManager: {
+              size,
+              location,
+              routeConfig,
+              isCompactLayout,
+              toggleMenu: this.toggleMenu,
+              menuIsOpen,
+            },
+          })
+        )}
+      </ContentContainer>
+    );
+  }
+
+  render() {
+    const { menuIsOpen, menuIsPinned, size } = this.state;
+    const { applicationToolbar, contentRoutingVessel, menuRoutingVessel, menuText, ...customProps } = this.props;
+
+    return (
+      <Layout
+        {...customProps}
+        header={this.renderApplicationToolbar()}
+        menu={this.renderMenu()}
+        menuText={menuText}
+      >
+        {contentRoutingVessel}
+      </Layout>
     );
   }
 }
 
 Navigation.propTypes = propTypes;
-Navigation.defaultProps = defaultProps;
-Navigation.breakpoints = BREAKPOINTS;
 
-export default Navigation;
+export default withRouter(Navigation);
